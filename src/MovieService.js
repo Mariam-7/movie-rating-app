c2bcd439124539191492292e21bb88cf9b08dc56
Areej Kamran <areej.kamran1@ontariotechu.net>
2025-04-09 13:43:52 -0400

    Resolved merge conflict in MovieService.js

--- src/MovieService.js
+++ src/MovieService.js
@@ -1,4 +1,4 @@
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY//MAKE SURE TO ADD UR OWN API KEY
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY //MAKE SURE TO ADD UR OWN API KEY
 const BASE_URL = 'https://api.themoviedb.org/3'
 
 export const searchMovies = async (query) => {
